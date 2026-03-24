7c5a531a67f84aaca7ae0177f6e731eb67843ec7
Onur Arda Özçimen <ardaozcimen@yahoo.com>
2026-03-24 23:38:15 +0300

    set new folder path

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -1,34 +1,17 @@
-package java.core;
+package core;
+
 import database.Database;
 import database.DbStatus;
 import java.sql.ResultSet;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Let's get the party started!");
 
-        // Let's try to connect to our database
+        
         Database myDB = new Database();
-        if (myDB.connect() == DbStatus.SUCCESS){
-            System.out.println("Done!");
-        }
-        else{
-            System.out.println("Error!");
-        }
-
-        // myDB.addUser("Ada", 20);
-        // myDB.deleteUser("Ada");
-        ResultSet myResults = myDB.getAllUsers();
-        try{
-            while (myResults.next()) {
-                System.out.println(
-                    myResults.getInt("id") + " | " +
-                    myResults.getString("name") + " | " +
-                    myResults.getInt("age")
-                );
-            }
-        }catch (Exception e) {
-            e.getStackTrace();
+        if (myDB.connect() == DbStatus.SUCCESS) {
+             System.out.println("Database connection established!");
         }
     }
 }
