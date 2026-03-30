35cb9ce42fff89add47571d785ea5ed8fda05b8c
DejaVu <ilkanseckin@gmail.com>
2026-03-30 15:48:33 +0300

    Add Verification code & eMail sender

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module bilfit {
     requires javafx.fxml;
     requires java.sql; // Database kullandığın için
     requires org.postgresql.jdbc;
+    requires jakarta.mail;
 
     opens core to javafx.graphics, javafx.fxml;
     opens controllers to javafx.fxml;
