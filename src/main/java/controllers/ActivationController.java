855b8c2f7154be2de590b1ff847637492afb4b63
Göktan Arslan <ghgarslan@gmail.com>
2026-04-02 09:40:48 +0300

    Added activation code check and redirection to ActivationView in ResetPasswordController.

--- src/main/java/controllers/ActivationController.java
+++ src/main/java/controllers/ActivationController.java
@@ -1,9 +1,15 @@
 package controllers;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class ActivationController {
 
@@ -23,6 +29,17 @@ public class ActivationController {
 
     @FXML
     public void goBack(MouseEvent event) {
-        System.out.println("Redirecting to Previous Screen...");
+        System.out.println("Redirecting to ResetPassword Screen");
+        try {
+            // 1. Yeni FXML dosyasını yükle (Yolun doğru olduğundan emin ol)
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/auth/ResetPasswordView.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.getScene().setRoot(root);
+
+        } catch (IOException e) {
+            System.err.println("ResetPasswordView yüklenirken hata oluştu!");
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/controllers/ResetPasswordController.java
+++ src/main/java/controllers/ResetPasswordController.java
@@ -1,12 +1,15 @@
 package controllers; import java.io.IOException;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 public class ResetPasswordController {
+    @FXML private TextField emailField;
     @FXML public void initialize() {} 
     @FXML
     public void goBack(MouseEvent event) {
@@ -24,8 +27,31 @@ public class ResetPasswordController {
         }
     }
     @FXML
-    public void attemptLogin(MouseEvent event) {
+    public void attemptLogin(ActionEvent event) {
         System.out.println("Attempting to reset password...");
+        String mail=emailField.getText();
+        /* TODO: Mail'i database'de bul ve doğrula
+                if (mail.isEmpty()) {
+            System.out.println("Error: Activation code cannot be empty.");
+            return;
+        }
+            */
+           System.out.println("Mail doğrulandı, ActivationView'e yönlendiriliyor...");
+           checkActivationCode(event);
+    }
+        public void checkActivationCode(ActionEvent event) {
+        System.out.println("Redirecting to ActivationView");
+         try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/auth/ActivationView.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.getScene().setRoot(root);
+
+        } catch (IOException e) {
+            System.err.println("ActivationView yüklenirken hata oluştu!");
+            e.printStackTrace();
+        }
+        
     }
 
 
