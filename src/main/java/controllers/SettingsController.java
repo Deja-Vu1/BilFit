92e1bc56b9f783638fd2278c03c177abe283e61d
Göktan Arslan <ghgarslan@gmail.com>
2026-04-03 11:27:38 +0300

    fixed toogleswitch

--- src/main/java/controllers/SettingsController.java
+++ src/main/java/controllers/SettingsController.java
@@ -1,9 +1,27 @@
 package controllers; import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
+import org.controlsfx.control.ToggleSwitch;
 public class SettingsController { 
     @FXML private PasswordField passwordField;
-    @FXML public void initialize() {} 
+    @FXML private ToggleSwitch publicAccountSwitch; // <-- Inject switch
+    @FXML private ToggleSwitch eloSwitch;
+    @FXML public void initialize() {
+        // Example: Listen for changes on the Public Account switch
+        publicAccountSwitch.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            System.out.println("Public Account is now: " + (newValue ? "ON" : "OFF"));
+            // TODO: Update database or settings manager
+        });
+
+        // Example: Listen for changes on the ELO switch
+        eloSwitch.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            System.out.println("ELO is now: " + (newValue ? "ON" : "OFF"));
+            // TODO: Update database or settings manager
+        });
+        
+        // If you need to set their default states from the database when loading:
+        // publicAccountSwitch.setSelected(true);
+    }
     @FXML
         public void resetPassword(ActionEvent event) {
         System.out.println("Attempting to reset password...");
@@ -16,9 +34,4 @@ public class SettingsController {
             */
            System.out.println("Şifre değiştirildi");
     }
-    @FXML public void makeAccountPublic(ActionEvent event) {
-        System.out.println("Returning to main menu...");
-    }
-
-
 }
