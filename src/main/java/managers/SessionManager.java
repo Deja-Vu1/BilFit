35c871d9caa5493e1bb19707d81ed7bfb5b337b9
Samet Talha Zorlu <talhazorlu2006@gmail.com>
2026-04-03 20:53:37 +0300

    implemented tournament controller

--- src/main/java/managers/SessionManager.java
+++ src/main/java/managers/SessionManager.java
@@ -15,6 +15,8 @@ public class SessionManager {
 
     // 3. ELO sayfasındaki "Request" butonunun hafızası (Sayfa değişse de unutmasın diye)
     private boolean isDuelloRequested = false;
+    private boolean isTournamentApplied = false;
+    private boolean isTournamentJoinedWithCode = false;
 
     private SessionManager() {}
 
@@ -57,12 +59,19 @@ public class SessionManager {
     public void setDuelloRequested(boolean requested) {
         this.isDuelloRequested = requested;
     }
+    public boolean isTournamentApplied() { return isTournamentApplied; }
+    public void setTournamentApplied(boolean applied) { this.isTournamentApplied = applied; }
 
+    public boolean isTournamentJoinedWithCode() { return isTournamentJoinedWithCode; }
+    public void setTournamentJoinedWithCode(boolean joined) { this.isTournamentJoinedWithCode = joined; }
     // --- ÇIKIŞ YAPMA (TÜM HAFIZAYI TEMİZLEME) ---
     public void logout() {
         this.currentUser = null;
         this.currentReservation = null;
         this.isDuelloRequested = false; // Çıkış yapınca buton hafızası da sıfırlanır
         this.isJoinedWithCode = false;
+        this.isTournamentApplied = false;
+        this.isTournamentJoinedWithCode = false;
     }
+    
 }
