82e8fa083a3f1808772f1135b9c0a51649c1ede4
DejaVu <ilkanseckin@gmail.com>
2026-04-04 15:53:53 +0300

    Modify Admin model

--- src/main/java/models/Admin.java
+++ src/main/java/models/Admin.java
@@ -1,13 +1,24 @@
 package models;
 
 public class Admin extends User {
-    private String activationCode;
+    private int actionPerformed;
 
-    public Admin(String fullName, String bilkentEmail, String activationCode) {
+    public Admin(String fullName, String bilkentEmail, int actionsPerformed) {
         super(fullName, bilkentEmail);
-        this.activationCode = activationCode;
+        this.actionPerformed = actionsPerformed;
+    }
+    public int getActionPerformed() {
+        return actionPerformed;
+    }
+    public void setActionPerformed(int actionPerformed) {
+        this.actionPerformed = actionPerformed;
     }
 
-    public String getActivationCode() { return activationCode; }
-    public void setActivationCode(String activationCode) { this.activationCode = activationCode; }
+    public String toString() {
+        return "Admin{" +
+                "fullName='" + getFullName() + '\'' +
+                ", bilkentEmail='" + getBilkentEmail() + '\'' +
+                ", actionPerformed=" + actionPerformed +
+                '}';
+    }
 }
